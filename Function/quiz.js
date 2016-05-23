1b32dc69b88056651ffe26ef9700204b2ac95207
daixiao3960 <328549705@qq.com>
2016-05-23 23:24:35 +1200

    Add files via upload

---
+++ Function/quiz.js
@@ -0,0 +1,141 @@
+﻿(function($) {
+    $.fn.jquizzy = function(settings) {
+        var defaults = {
+            questions: null,
+            startImg: 'images/start.gif',
+            shortURL: null,
+            sendResultsURL: null,
+            resultComments: {
+
+            }
+        };
+        var config = $.extend(defaults, settings);
+        if (config.questions === null) {
+            $(this).html('<div class="intro-container slide-container"><h2 class="qTitle">Failed to parse questions.</h2></div>');
+            return;
+        }
+        var superContainer = $(this),
+        answers = [],
+        introFob = '<div class="intro-container slide-container"><a class="nav-start" href="#">Please take some mintues to finish the question<br/><br/><span><img src="'+config.startImg+'"></span></a></div>	',
+        exitFob = '<div class="results-container slide-container"<a class="nav-start" href="#">Thanks for patience, we are finding a buddy for you!<div class="question-number"></div></a><div class="result-keeper"></div></div><div class="notice">Select an option please!</div><div class="progress-keeper" ><div class="progress"></div></div>',
+        contentFob = '',
+        questionsIteratorIndex,
+        answersIteratorIndex;
+        superContainer.addClass('main-quiz-holder');
+        for (questionsIteratorIndex = 0; questionsIteratorIndex < config.questions.length; questionsIteratorIndex++) {
+            contentFob += '<div class="slide-container"><div class="question-number">' + (questionsIteratorIndex + 1) + '/' + config.questions.length + '</div><div class="question">' + config.questions[questionsIteratorIndex].question + '</div><ul class="answers">';
+            for (answersIteratorIndex = 0; answersIteratorIndex < config.questions[questionsIteratorIndex].answers.length; answersIteratorIndex++) {
+                contentFob += '<li>' + config.questions[questionsIteratorIndex].answers[answersIteratorIndex] + '</li>';
+            }
+            contentFob += '</ul><div class="nav-container">';
+            if (questionsIteratorIndex !== 0) {
+                contentFob += '<div class="prev"><a class="nav-previous" href="#">&lt; Back</a></div>';
+            }
+            if (questionsIteratorIndex < config.questions.length - 1) {
+                contentFob += '<div class="next"><a class="nav-next" href="#">Next &gt;</a></div>';
+            } else {
+                contentFob += '<div class="next final"><a class="nav-show-result" href="#">Finish</a></div>';
+            }
+            contentFob += '</div></div>';
+            answers.push(config.questions[questionsIteratorIndex].correctAnswer);
+        }
+        superContainer.html(introFob + contentFob + exitFob);
+        var progress = superContainer.find('.progress'),
+        progressKeeper = superContainer.find('.progress-keeper'),
+        notice = superContainer.find('.notice'),
+        progressWidth = progressKeeper.width(),
+        userAnswers = [],
+        questionLength = config.questions.length,
+        slidesList = superContainer.find('.slide-container');
+        function checkAnswers() {
+            var resultArr = [],
+            flag = false;
+            for (i = 0; i < answers.length; i++) {
+                if (answers[i] == userAnswers[i]) {
+                    flag = true;
+                } else {
+                    flag = false;
+                }
+                resultArr.push(flag);
+            }
+            return resultArr;
+        }
+        function roundReloaded(num, dec) {
+            var result = Math.round(num * Math.pow(10, dec)) / Math.pow(10, dec);
+            return result;
+        }
+        progressKeeper.hide();
+        notice.hide();
+        slidesList.hide().first().fadeIn(500);
+        superContainer.find('li').click(function() {
+            var thisLi = $(this);
+            if (thisLi.hasClass('selected')) {
+                thisLi.removeClass('selected');
+            } else {
+                thisLi.parents('.answers').children('li').removeClass('selected');
+                thisLi.addClass('selected');
+            }
+        });
+        superContainer.find('.nav-start').click(function() {
+            $(this).parents('.slide-container').fadeOut(500,
+            function() {
+                $(this).next().fadeIn(500);
+                progressKeeper.fadeIn(500);
+            });
+            return false;
+        });
+        superContainer.find('.next').click(function() {
+            if ($(this).parents('.slide-container').find('li.selected').length === 0) {
+                notice.fadeIn(300);
+                return false;
+            }
+            notice.hide();
+            $(this).parents('.slide-container').fadeOut(500,
+            function() {
+                $(this).next().fadeIn(500);
+            });
+            progress.animate({
+                width: progress.width() + Math.round(progressWidth / questionLength)
+            },
+            500);
+            return false;
+        });
+        superContainer.find('.prev').click(function() {
+            notice.hide();
+            $(this).parents('.slide-container').fadeOut(500,
+            function() {
+                $(this).prev().fadeIn(500);
+            });
+            progress.animate({
+                width: progress.width() - Math.round(progressWidth / questionLength)
+            },
+            500);
+            return false;
+        });
+        superContainer.find('.final').click(function() {
+            if ($(this).parents('.slide-container').find('li.selected').length === 0) {
+                notice.fadeIn(300);
+                return false;
+            }
+            superContainer.find('li.selected').each(function(index) {
+                userAnswers.push($(this).parents('.answers').children('li').index($(this).parents('.answers').find('li.selected')) + 1);
+            });
+            if (config.sendResultsURL !== null) {
+                var collate = [];
+                for (r = 0; r < userAnswers.length; r++) {
+                    collate.push('{"questionNumber":"' + parseInt(r + 1, 10) + '", "userAnswer":"' + userAnswers[r] + '"}');
+                }
+                $.ajax({
+                    type: 'POST',
+                    url: config.sendResultsURL,
+                    data: '{"answers": [' + collate.join(",") + ']}',
+                    complete: function() {
+                        console.log("OH HAI");
+                    }
+                });
+            }
+            progressKeeper.hide();
+            
+        });
+    };
+})(jQuery);
